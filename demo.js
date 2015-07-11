60b0c070416f7fb100136e055d1d13aa8da9917e
thirdcoder <boris3rd@bk.ru>
2015-07-10 21:14:59 -0700

    Random trits

--- demo.js
+++ demo.js
@@ -6,6 +6,7 @@ window.t = Tricanvas();
 console.log(t);
 
 for (var i = 0; i < 81*81*5; ++i)
-  t.tritmap[i] = 80; // 1000i
+  t.tritmap[i] = (Math.random()*241-121)|0;
+  //t.tritmap[i] = 80; // 1000i
 
 t.refresh();
